4146ca5cd4f3ed7905d0103dcc297673735437c0
Siggyy1 <jorgenandre21@gmail.com>
2023-09-20 18:24:09 +0200

    load tasks from server

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>My react Task application on Heroku</h1>);
+
+function TaskApplication(){
+    const [tasks, setTasks] = useState([]);
+
+    async function loadTasks() {
+        const res = await fetch("api/tasks");
+        setTasks(await res.json());
+    }
+
+    useEffect(() => {
+        loadTasks();
+    }, []);
+
+    return <>
+        <h1>This is a React Task application on Heroku</h1>
+        {tasks.map(t => <div>{t.title}</div>)}
+    </>;
+}
+
+root.render(<TaskApplication/>);
