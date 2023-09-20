b6041813ca0b4337e2c1dd342dff8a693188fcaa
Siggyy1 <jorgenandre21@gmail.com>
2023-09-20 14:04:45 +0200

    Now running with React

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>My react Task application!</h1>)
